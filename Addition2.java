a034f06a413f26faabf264bca6b6fd24c8a593b7
madhavkumarpeetani <151707152+madhavkumarpeetani@users.noreply.github.com>
2024-08-22 12:41:27 +0530

    Create Addition2.java

---
+++ Addition2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Addition2
+{
+public static void main(String args[])
+{
+int x=Integer.parseInt(args[0]);
+int y=Integer.parseInt(args[1]);
+int z=x+y;
+System.out.println("Sum of"+x+ "and"+y+ "is"+z);
+}
+}
