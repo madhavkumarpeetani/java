7b60e54420a070d36b90ec9ef732026c98e7459e
madhavkumarpeetani <151707152+madhavkumarpeetani@users.noreply.github.com>
2024-08-22 12:52:16 +0530

    Create Thisdemo.java

---
+++ Thisdemo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Thisdemo
+{
+	    private int a=50;
+	    public void setNumber(int a)
+	    {
+	        this.a=a;
+	        System.out.println("a value is  "+a);
+	    }
+	    public static void main(String args[])
+	    {
+	        Thisdemo td=new Main();
+	        td.setNumber(60);
+	    }
+    
+}
