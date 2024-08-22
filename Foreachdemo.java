c15e69f9ff314f2e943448153e4539ec4547f7f4
madhavkumarpeetani <151707152+madhavkumarpeetani@users.noreply.github.com>
2024-08-22 11:31:16 +0530

    Create Foreachdemo.java

---
+++ Foreachdemo.java
@@ -0,0 +1,11 @@
+public class Foreachdemo
+{
+	public static void main(String[] args) {
+	   int arr[]={10,20,30,40,50};
+	   System.out.println("The array elements are : ");
+	   for(int i:arr)
+	   {
+	       System.out.println(i+" ");
+	   }
+	}
+}
