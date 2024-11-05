68fcb3df26b16549beb31fd93ea5685d1e16a4f8
madhavkumarpeetani <151707152+madhavkumarpeetani@users.noreply.github.com>
2024-11-05 15:33:35 +0530

    Create Superdemo.java

---
+++ Superdemo.java
@@ -0,0 +1,21 @@
+class Animal
+{
+	String color="White";
+}
+class Dog extends Animal
+{
+	String color="Black";
+	void printcolor()
+	{
+		System.out.println("Dog color is "+color);
+		System.out.println("Animal color is "+super.color);
+	}
+}
+class Superdemo
+{
+	public static void main(String args[])
+	{
+		Dog d=new Dog();
+		d.printcolor();
+	}
+}
